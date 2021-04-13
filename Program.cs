44084d38b89be26139b8306b43578e5056b566e4
Isaac <isaacgeazy@gmail.com>
2021-04-13 11:17:03 -0300

    Divisaoaoao

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num, den, resu;
+            
+            Console.Write("Digite o numerador....: ");
+            num = Int16.Parse(Console.ReadLine());
+
+            Console.Write("Digite o denominador..:");
+            den = Int16.Parse(Console.ReadLine());
+
+            
+            resu = num / den;
+
+            if (den > 0) {
+          
+          
+             Console.WriteLine($"{num} dividido por {den} é {resu}");
+
+            }else{
+
+  
+              Console.WriteLine("Não é possível dividir por zero.");
+
+            }
+
+
+
+        }
+    }
+}
